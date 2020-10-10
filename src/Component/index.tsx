870c2b3538968ce3e20dfc5e8d88f7058cebb76e
Alireza <alireza.mh@gmail.com>
2020-10-10 14:54:19 +0330

    feat: support bring 0 to 9 with same animation

--- src/Component/index.tsx
+++ src/Component/index.tsx
@@ -12,8 +12,8 @@ interface IAllDigits extends React.HTMLProps<HTMLDivElement> {
 }
 
 const AllDigits: React.FC<IAllDigits> = (props: IAllDigits) => {
-    const {style, ...rest} = props;
-    return (<div className='react-timer_digit-container' style={style} {...rest}>
+    const {style, className,  ...rest} = props;
+    return (<div className={`react-timer_digit-container ${className}` } style={style} {...rest}>
         <div>0</div>
         <div>1</div>
         <div>2</div>
@@ -24,20 +24,31 @@ const AllDigits: React.FC<IAllDigits> = (props: IAllDigits) => {
         <div>7</div>
         <div>8</div>
         <div>9</div>
+        {/* little hack to animate from zero to nine again without scroll all way down*/}
+        <div>0</div>
     </div>);
 }
 /**
  * @param props: ITimerProps
  */
 const Timer: React.FC<ITimerProps> = (props: ITimerProps) => {
-    const [time, updateTime] = useState<number>(9);
+    const [time, updateTime] = useState<number>(30);
+    const [removeAnimation, setRemoveAnimation] = useState<boolean>(false);
     useEffect(() => {
         setInterval(() => updateTime(prevTime => prevTime === 0 ? 0  : prevTime - 1) , 1000)
-    }, [])
+    }, []);
+    if(time%10 === 0){
+        setTimeout(() => setRemoveAnimation(true), 500);
+    } else{
+        if(removeAnimation) {
+            setRemoveAnimation(false);
+        }
+    }
+
     return (
         <div className='react-timer'>
             <div className='react-timer-decimal'>
-                <AllDigits style={{marginTop: `${-(23*time)}px`}}/>
+                <AllDigits className={removeAnimation ? 'remove-animation' : ''} style={{marginTop: removeAnimation ? `${-(23*(10))}px` : `${-(23*(time%10))}px`}}/>
             </div>
         </div>
     );
